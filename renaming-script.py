7862cf87a301c1486caffeae4cb937e783c962e2
Bryant Wooters <Jesusrox45@gmail.com>
2020-06-10 21:47:25 -0400

    Add files via upload

--- renaming-script.py
+++ renaming-script.py
@@ -7,31 +7,24 @@ import os
 #importing regex
 import re 
 
-#Andrew's code
-def renameFile(inputFileName):
-    return "-".join(re.sub( r"([A-Z])"), r" \1", inputFileName).split()
-
-files = ["FinalExamSchedule", "FinalExam", "2020FallRegistration", "Spring2021Schedule"]
-[print(renameFile(i)) for i in files]
-#end of Andrew's code
-
 # Function to rename multiple files 
 
 def main():
     for count, filename in enumerate(os.listdir(r"C:\Users\jesus\Documents\Work")):   
-        #separateWords = renameFile(filename)
-        spacesToDashes = filename.replace(" ", "-")
-        underscoreToDashes = filename.replace("_", "-")
-        oldFileName = filename  
+        oldFileName = filename
+        filename = "-".join(re.sub(r"([A-Z])", r" \1", str(filename)).split()).replace(" ", "-").replace("_", "-").replace("--", "-")
         # rename() function will 
         # rename all the files 
-        #os.rename((os.path.join(r"C:\Users\jesus\Documents\Work", oldFileName)), os.path.join(r"C:\Users\jesus\Documents\Work", separateWords))
-        os.rename((os.path.join(r"C:\Users\jesus\Documents\Work", oldFileName)), os.path.join(r"C:\Users\jesus\Documents\Work", spacesToDashes)) 
-        os.rename((os.path.join(r"C:\Users\jesus\Documents\Work", oldFileName)), os.path.join(r"C:\Users\jesus\Documents\Work", underscoreToDashes))
+        print("Old name:\t" + oldFileName)
+        print("New name:\t" + filename)
+        # os.rename((os.path.join(r"C:\Users\jesus\Documents\Work", oldFileName)), os.path.join(r"C:\Users\jesus\Documents\Work", filename))
+        # os.rename((os.path.join(r"C:\Users\jesus\Documents\Work", oldFileName)), os.path.join(r"C:\Users\jesus\Documents\Work", spacesToDashes)) 
+        # os.rename((os.path.join(r"C:\Users\jesus\Documents\Work", oldFileName)), os.path.join(r"C:\Users\jesus\Documents\Work", underscoreToDashes))
 
 
 
-# Driver Code 
-if __name__ == '__main__': 
+
+# # Driver Code 
+if __name__ == '__main__':
     # Calling main() function 
     main()
